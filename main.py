076f76340745e3139305035adc92e49bb6f41cfe
Jon Azpiazu <jonazpiazu@users.noreply.github.com>
2023-08-23 11:45:09 +0200

    Add option to download all latest podcasts from all urls in the config files

--- main.py
+++ main.py
@@ -36,9 +36,20 @@ args = parser.parse_args()
 
 
 def main(podcast_name, episode_name, latest_episode):
-    podcast_name = podcast_name[0] if podcast_name else None
-    episode_name = episode_name[0] if episode_name else None
-    dp = DownloadPodcast(podcast_name, episode_name, latest_episode)
-    dp.download_episode()
+    if len(podcast_name) > 1:
+        for single_podcast_name in podcast_name:
+            dp = DownloadPodcast(single_podcast_name, None, True)
+            dp.download_episode()
+    elif podcast_name[0] == 'all':
+        from src.config import Config
+        config = Config()
+        for podcast_name in config.get_podcast_keys():
+            dp = DownloadPodcast(podcast_name, None, True)
+            dp.download_episode()
+    else:
+        podcast_name = podcast_name[0] if podcast_name else None
+        episode_name = episode_name[0] if episode_name else None
+        dp = DownloadPodcast(podcast_name, episode_name, latest_episode)
+        dp.download_episode()
 
 main(args.p, args.e, args.latest)
